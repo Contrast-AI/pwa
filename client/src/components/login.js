c0222cc505b36e76661f6db655c41db5c1cca886
Aidan Aug <aidanaug@Aidans-MacBook-Pro.local>
2022-09-30 01:31:52 -0400

    formats and cleans code

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -7,23 +7,20 @@ function Login() {
   return (
     <div class="full-screen-container">
       <div class="login-box">
-        {/*<div class="h1 text-center"> ContrastAI</div> replace with logo */}
-          <img class="logo" src={require('../images/ContrastLogo.png')} alt="ContrastLogo"/>
-          <form class= "form">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" id="username" aria-describedby="usernameHelp" placeholder="Enter Username"/>
-              
-            </div>
-            
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" id="password" placeholder="Enter Password"/>
-            </div>
-            <button type="submit" class="login-button">Submit</button>
-          </form>
-        </div>
-  
+        <img class="logo" src={require("../images/ContrastLogo.png")} alt="ContrastLogo"/>
+        <form class="form">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" placeholder="Enter Username"/>
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" placeholder="Enter Password" />
+          </div>
+          <button type="submit" class="login-button">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
